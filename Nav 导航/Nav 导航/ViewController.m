d702756792fe2982e74e9f56d580cccb9759cf96
majunliang <amjunliang@163.com>
2017-12-21 04:27:51 +0800

    no message

--- Nav 导航/Nav 导航/ViewController.m
+++ Nav 导航/Nav 导航/ViewController.m
@@ -19,12 +19,22 @@
     // Do any additional setup after loading the view, typically from a nib.
     
     self.title = @"test";
+    //return;
     
     if (!(self.navigationController.viewControllers.count == 1)) {
         self.navigationItem.backBarButtonItem = [[UIBarButtonItem alloc]initWithTitle:@"title" style:UIBarButtonItemStylePlain target:nil action:nil];
         self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc]initWithTitle:@"title" style:UIBarButtonItemStylePlain target:nil action:nil];
         self.navigationController.interactivePopGestureRecognizer.delegate = (id)self;
     }
+    
+    UIView *v = [UIView new];
+    v.frame = CGRectMake(0, 0, 100, 100);
+    v.backgroundColor = [UIColor redColor];
+    
+    
+    
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc]initWithCustomView:v];
+
 }
 
 
